b5522a8919dbdb63fa30cf8ee55f99a5e9bd5fbd
quhou <yingxiang.cao@autobizline.com>
2025-11-26 14:09:25 +0000

    feat: add all feature json file

--- theme/views.py
+++ theme/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 logger = logging.getLogger("logview.project")
 def wavespeed_feature(request, keyword = 'remove_objects_from_photos'):
     json_name = f'{keyword}'
-    json_path = f'internal/wavespeed_data/feature/{json_name}.json'
+    json_path = f'internal/feature/{json_name}.json'
     logger.info(f"Loading wavespeed feature-{keyword} data from: {json_path}")
     with open(json_path,'r', encoding='utf-8') as f:
         data = json.load(f)
